5821e30fa3b189d2e9371ecad876a27d5073ef53
cheeseandpepper <cheeseandpepper@gmail.com>
2015-09-07 10:28:30 -0400

    using rails asset helper method in the Icon component

--- spec/models/tweet_object_parser_spec.rb
+++ spec/models/tweet_object_parser_spec.rb
@@ -14,10 +14,10 @@ describe TweetObjectParser do
       allow(mock_user_object).to receive(:followers_count        ) { 234            }
       allow(mock_user_object).to receive(:profile_image_url_https) { 'https://pbs.twimg.com/profile_images/637229465152581632/rHZ0lrIJ_normal.jpg' }
       
-      allow(mock_object).to receive(:id                    ) { 639921861241864193 }
-      allow(mock_object).to receive(:favorite_count        ) { 123                }
-      allow(mock_object).to receive(:retweet_count         ) { 234                }
-      allow(mock_object).to receive(:text                  ) { "@billboard that's because they are the best. Stuck with me. Love my #beliebers" }
+      allow(mock_object).to receive(:id            ) { 639921861241864193 }
+      allow(mock_object).to receive(:favorite_count) { 123                }
+      allow(mock_object).to receive(:retweet_count ) { 234                }
+      allow(mock_object).to receive(:text          ) { "@billboard that's because they are the best. Stuck with me. Love my #beliebers" }
 
       expect(instance.new(mock_object).data).to eq(expected_object)
     end
@@ -27,13 +27,13 @@ describe TweetObjectParser do
 
   def expected_object
     {
-      image_url:   "https://pbs.twimg.com/profile_images/637229465152581632/rHZ0lrIJ_normal.jpg",
-      screen_name: "justinbieber",
-      text:        "<a href='http://twitter.com/billboard' target='_blank'>@billboard</a> that's because they are the best. Stuck with me. Love my #beliebers",
-      id:          639921861241864193,
+      image_url:       "https://pbs.twimg.com/profile_images/637229465152581632/rHZ0lrIJ_normal.jpg",
+      screen_name:     "justinbieber",
+      text:            "<a href='http://twitter.com/billboard' target='_blank'>@billboard</a> that's because they are the best. Stuck with me. Love my #beliebers",
+      id:              639921861241864193,
       followers_count: 234,
-      retweet_count: 234,
-      favorite_count: 123
+      retweet_count:   234,
+      favorite_count:  123
     }
   end
 end
